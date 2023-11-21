f8948fd2daf0600e1970e3e0ea9332317320193c
murphj42 <murphj42@outlook.ie>
2023-11-21 21:16:45 +0000

    Initial commit using Spring Boot initializer

---
+++ src/main/java/com/example/joespetitions/JoespetitionsApplication.java
@@ -0,0 +1,13 @@
+package com.example.joespetitions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JoespetitionsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JoespetitionsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/joespetitions/JoespetitionsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.joespetitions;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JoespetitionsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
