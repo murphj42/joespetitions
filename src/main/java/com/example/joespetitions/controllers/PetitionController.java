03c234f7179eca99fa2520da5a61dd9bb1ec0074
murphj42 <murphj42@outlook.ie>
2023-11-23 16:13:16 +0000

    Added comments

--- src/main/java/com/example/joespetitions/controllers/PetitionController.java
+++ src/main/java/com/example/joespetitions/controllers/PetitionController.java
@@ -18,13 +18,14 @@ public class PetitionController implements ApplicationRunner {
 
     private List<Petition> petitions = new ArrayList<>(); // Your data structure to store petitions
 
-
+    // Control navigation to /create_petition page
     @GetMapping("/create_petition")
     public String showCreatePetitionForm(Model model) {
         model.addAttribute("petition", new Petition());
         return "create_petition";
     }
 
+    /* logic to create unique identifier and add new petition to existing list */
     @PostMapping("/create_petition")
     public String createPetition(@ModelAttribute Petition petition) {
         petition.setId(UUID.randomUUID().toString()); // Generate a unique ID
@@ -32,12 +33,14 @@ public class PetitionController implements ApplicationRunner {
         return "redirect:/view_all";
     }
 
+    /* Control navigation to 'view_all' page */
     @GetMapping("/view_all")
     public String showAllPetitions(Model model) {
         model.addAttribute("petitions", petitions);
         return "view_all";
     }
 
+    /* control navigation to 'view_petition' page - find all petitions and add to request */
     @GetMapping("/view_petition")
     public String viewPetition(@RequestParam("id") String id, Model model) {
         Petition petition = findPetitionById(id);
@@ -46,11 +49,12 @@ public class PetitionController implements ApplicationRunner {
             model.addAttribute("signee", new User()); // For the signee form
             return "view_petition";
         } else {
-            // Handle case where petition is not found
+            // handle case where petition is not found
             return "redirect:/view_all";
         }
     }
 
+    /* logic to allow a user to sign a petition and add them to singee list */
     @PostMapping("sign_petition")
     public String signPetition(@RequestParam("id") String id, @ModelAttribute User signee, Model model) {
         Petition petition = findPetitionById(id);
@@ -61,12 +65,14 @@ public class PetitionController implements ApplicationRunner {
         return "redirect:/view_petition?id=" + id;
     }
 
+    /* control navigation to 'view_petition' page */
     @GetMapping("/search_petitions")
     public String searchPetitions(Model model) {
 
         return "search_petitions";
     }
 
+    /* logic to allow user to enter a search term(s) and find a petition in the list */
     @PostMapping("/search_petitions")
     public String searchPetitions(@RequestParam String searchTerm, Model model) {
         List<Petition> searchResults = searchPetitionsByTerm(searchTerm);
@@ -75,7 +81,7 @@ public class PetitionController implements ApplicationRunner {
     }
 
 
-    // Helper method to search petitions by a term
+    // helper method to search petitions by a term
     private List<Petition> searchPetitionsByTerm(String searchTerm) {
         // search by petition name or description
         return petitions.stream()
@@ -87,7 +93,7 @@ public class PetitionController implements ApplicationRunner {
     }
 
 
-    // Helper method to find a petition by ID
+    // helper method to find a petition by ID
     private Petition findPetitionById(String id) {
         return petitions.stream()
                 .filter(p -> p.getId().equals(id))
@@ -95,6 +101,9 @@ public class PetitionController implements ApplicationRunner {
                 .orElse(null);
     }
 
+    /* implementation of ApplicationRunner
+    *       Creates sample data for application and executes when it is first run
+    *  */
     @Override
     public void run(ApplicationArguments args) throws Exception {
         List<Petition> samplePetitions = generateSamplePetitions();
@@ -103,6 +112,8 @@ public class PetitionController implements ApplicationRunner {
 
     private List<Petition> generateSamplePetitions() {
         List<Petition> samplePetitions = new ArrayList<>();
+
+        // Create Petition object and add to ArrayList
         Petition p1 = new Petition();
         p1.setName("Save the Bees");
         p1.setDescription("Protect the bee population for a sustainable environment.");
@@ -115,6 +126,7 @@ public class PetitionController implements ApplicationRunner {
         p2.setId(UUID.randomUUID().toString());
         samplePetitions.add(p2);
 
+        // Create User object and ass them as a singee for petitions we have created
         User u1 = new User();
         u1.setName("Donald Trump");
         u1.setEmail("TheDonald@whitehouse.com");
